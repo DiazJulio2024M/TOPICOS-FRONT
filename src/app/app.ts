f2a706048eafc72a53b3c027a224eb1241d8103e
DiazJulio2024M <202010606@urp.edu.pe>
2025-12-05 13:22:14 -0500

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
+import { filter } from 'rxjs/operators';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-microservicios');
+
+  isAuthRoute = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+
+        const url = event.urlAfterRedirects;
+
+        this.isAuthRoute =
+          url.startsWith('/login') ||
+          url.startsWith('/registro');
+
+      });
+  }
 }
